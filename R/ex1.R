d6ee8c2c3a6b940dc699eb62fa7e56572d37f1c8
ilyalevner <elilevner@gmail.com>
2019-11-06 21:36:02 +0200

    R class 1

--- R/ex1.R
+++ R/ex1.R
@@ -11,6 +11,16 @@ fv<-factor(f,levels=c(1,0), labels=c("NO","YES"))
 
 help("objects")
 
-.Ob <- 1
-ls(pattern = "O")
-ls(pattern= "O", all.names = TRUE)    # also shows ".[foo]"
+
+R <- c(1,2,3,4,5)
+P <- c(1,1,3,4,3)
+Ti <- c('Avatar','Titanic','Star Wars','Avengers','Jurassic Prarc')
+W <- c(2787965,2187463,2068223,1844894,1671713)
+Y <-c(2009,1997,015,2018,2015)
+df <- data.frame(a,b,c,d,e)
+class(df)
+str(df)
+summary(df)
+
+df[2, 3]
+
